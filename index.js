0a410daa9c8855eda175ac113a7c13be9561ccbb
xinzhe <xinzhe1219@outlook.com>
2019-01-07 11:55:44 +0800

    New changes to original

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var bodyParser = require('body-parser');
+// Example express application adding the parse-server module to expose Parse
+// compatible API routes.
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var smsResponseWebhook = require('./sms_response_webhook');
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
@@ -14,28 +14,22 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'dog-facts-parse',
+  appId: process.env.APP_ID || 'myAppId',
   clientKey: process.env.CLIENT_KEY || '', 
   javascriptKey: process.env.JAVASCRIPT_KEY || '', 
   restAPIKey: process.env.RESTAPI_KEY || '', 
-  dotNetKey: 'dot-facts-123456',
-  masterKey: process.env.MASTER_KEY || '', 
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', 
-  push: {
-  ios: [
-    {
-      pfx: './p12/apns_dev.p12', // Prod PFX or P12
-      bundleId: 'com.mjm.catfacts',
-      production: false // Prod
-    }
-  ]
+  dotNetKey: process.env.DOTNET_KEY || '',
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
 });
+// Client-keys like the javascript key or the .NET key are not necessary with parse-server
+// If you wish you require them, you can set them as options in the initialization above:
+// javascriptKey, restAPIKey, dotNetKey, clientKey
 
 var app = express();
-
-// Configure body parser
-app.use(bodyParser.urlencoded({ extended: false }));
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
@@ -46,14 +40,20 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('Make sure to star the parse-server repo on GitHub!');
+  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
-// SMS response webhook
-app.post('/api/sms-response', smsResponseWebhook);
+// There will be a test page available on the /test path of your server url
+// Remove this before launching your app
+app.get('/test', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/test.html'));
+});
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
-    console.log('parse-server running on port ' + port + '.');
+    console.log('parse-server-example running on port ' + port + '.');
 });
+
+// This will enable the Live Query real-time server
+ParseServer.createLiveQueryServer(httpServer);
