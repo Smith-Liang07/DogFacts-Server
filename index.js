262446703bde9ebc1e139948490540ea0d4ba149
xinzhe <xinzhe1219@outlook.com>
2019-01-13 12:46:04 +0800

    added push notification

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }/*,
+  },
   push: {
   ios: [
     {
@@ -32,7 +32,7 @@ var api = new ParseServer({
       production: false // Prod
     }
   ]
-  }*/
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
